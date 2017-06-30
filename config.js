d5f19362394ae6424af148137c8a62a5d07865c2
idan <idan.itzhak@gmail.com>
2017-06-30 23:58:44 +0300

    smallchange

--- config.js
+++
@@ -1,4 +0,0 @@
-exports = module.exports = {
-  uri_session_db: 'mongodb://admin:missundaztood@ds143542.mlab.com:43542/cyberlove-sessions',
-  uri_users_db: 'mongodb://admin:missundaztood@ds143532.mlab.com:43532/cyberlove-users'
-}
